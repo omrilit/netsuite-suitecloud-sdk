7b0678dd5b99484d707d8c2ffcf30d1fb160c8ee
Omri Litvak <omri.litvak@salto.io>
2020-03-26 10:04:00 +0200

    Make the package usable with ts, expose required classes in index.js
    (cherry picked from commit 42f3202c99263d523c261f93e34858b634c0c7a3)
    (cherry picked from commit 3e3d1954866bdb8077375fceb8f92a254ff9983e)
    (cherry picked from commit 9a404e2b88823d07ca3b4ada778600f6ffc17883)
    (cherry picked from commit d375839daca8a1f6616af2597e4702614a15c17d)
    (cherry picked from commit 06df678e0ffbfb8de8a7dc7430aaecd2d92e9229)
    (cherry picked from commit 4f3e8a4f5be59c5acba518dce752b4d0ec834d68)

---
+++ packages/node-cli/index.js
@@ -0,0 +1,10 @@
+module.exports = {
+	CommandsMetadataService: require('./src/core/CommandsMetadataService'),
+	CommandActionExecutor: require('./src/core/CommandActionExecutor'),
+	CommandInstanceFactory: require('./src/core/CommandInstanceFactory'),
+	CommandOptionsValidator: require('./src/core/CommandOptionsValidator'),
+	CommandOutputHandler: require('./src/core/CommandOutputHandler'),
+	CLIConfigurationService: require('./src/core/extensibility/CLIConfigurationService'),
+	AuthenticationService: require('./src/core/authentication/AuthenticationService'),
+	SDKOperationResultUtils: require('./src/utils/SDKOperationResultUtils'),
+};
